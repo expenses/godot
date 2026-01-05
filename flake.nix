945ce063c626c0a607148d80a057c856364dc2af
Ashley Ruglys <ashley.ruglys@gmail.com>
2026-01-05 21:43:45 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+      	devShells.run = with pkgs; mkShell {
+       		LD_LIBRARY_PATH = lib.makeLibraryPath  [
+         fontconfig
+                     wayland
+                     libxkbcommon
+                     libGL
+
+                     xorg.libX11
+                     xorg.libXcursor
+                     xorg.libXi
+                     xorg.libXrandr
+                     xorg.libXext
+                     xorg.libXinerama
+                     libdecor
+                     dbus
+                     vulkan-loader
+                     vulkan-headers
+         ];
+
+       };
+      }
+    );
+}
